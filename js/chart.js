7efc3fa8e067189a439f28d8ad306c86a787d555
John Cokos <highlander.44@gmail.com>
2023-04-19 11:05:39 -0700

    Changes PD: Separate Chart Page

---
+++ js/chart.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let canvasElem = document.getElementById('chart')
+
+
+// TODO: Bring in the AppState, load product data from local storage and create the chart
+function renderChart() {
+}
+
+renderChart();
