7a8d64d8348d17cb1d6dc0fc04e95ade8bf45f43
Brook Riggio <brook@codefellows.com>
2023-04-19 23:38:42 -0700

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -2,8 +2,14 @@
 
 let canvasElem = document.getElementById('chart')
 
-
-// TODO: Bring in the AppState, load product data from local storage and create the chart
+/* TODO:
+ * - Instantiate a new AppState
+ * - Use a method on that AppState to load vote data from localStorage.
+ * - Create a data object for chart.js using your AppState's allProducts array.
+ * - Combine the data object with configuration information for chart.js type, colors, etc
+ * - Call chart.js with the configuration and the canvasElem
+ *
+ */
 function renderChart() {
 }
 
